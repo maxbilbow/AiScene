9a4f6f6fb0323737316f175e6418293e6fd833f3
maxbilbow <max@rattlemedia.co.uk>
2015-06-12 15:06:24 +0100

    Can enter full screen mode.

--- AiScene-Xcode/AiScene-OSX/AppDelegate.swift
+++ AiScene-Xcode/AiScene-OSX/AppDelegate.swift
@@ -13,16 +13,56 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     
     @IBOutlet weak var window: NSWindow!
     
+    var interface: RMXInterface! {
+        return (self.window.contentView.subviews.first as? GameView)?.interface
+    }
+    
+    #if DEBUG
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
+        var s = "\napplicationDidFinishLaunching"
+        s += "\n           World: \(self.interface.world.name)"
+        print(s, appendNewline: true)
     }
     
     func applicationWillHide(notification: NSNotification) {
-        
+        var s = "\napplicationWillHide"
+        self.interface.pauseGame()
+        s += "\n   Game was paused"
+        print(s, appendNewline: true)
+    }
+    
+    func applicationDidUnhide(notification: NSNotification) {
+        var s = "\napplicationDidUnhide"
+//        self.interface.pauseGame()
+        s += "\n   Game if not unpause automatically."
+        print(s, appendNewline: true)
     }
 
     func applicationDidChangeOcclusionState(notification: NSNotification) {
 //        NSLog(notification.description)
         
+        print("\napplicationDidChangeOcclusionState")
+        if self.window.occlusionState != NSWindowOcclusionState.Visible {
+            self.interface.pauseGame()
+        }
+        
     }
+    
+    func applicationDidBecomeActive(notification: NSNotification) {
+        print("\napplicationDidBecomeActive", appendNewLine: true)
+    }
+    
+    func applicationWillTerminate(notification: NSNotification) {
+        var s = "\napplicationWillTerminate"
+        s += "\n   Game will be archived?"
+        print(s, appendNewline: true)
+    }
+    
+    func applicationDidChangeScreenParameters(notification: NSNotification) {
+        print("\napplicationDidChangeScreenParameters", appendNewLine: true)
+    }
+    
+    
+    #endif
 }
